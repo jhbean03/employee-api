a86b621b228422d466a0affd1f646a51bba5a5a6
Jackson <159603141+jhbean03@users.noreply.github.com>
2025-02-24 02:07:53 -0500

    Removed logging file, start initialization in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import http from 'http'
 import express from 'express'
-import './config/logging';
 
 export const router = express();
 export let httpServer: ReturnType<typeof http.createServer>;
 
 export const Main = () => {
-    logging.info('-----------------------------------------------');
-    logging.info('Initializing API');
-    logging.info('-----------------------------------------------');
+    console.log('-----------------------------------------------');
+    console.log('Initializing API');
+    console.log('-----------------------------------------------');
+
+    // Configure express for parsing POST/DELETE requests
+    router.use(express.urlencoded({ extended: true}));
+    router.use(express.json());
+
+    console.log('-----------------------------------------------');
+    console.log('Logging & Configuration');
+    console.log('-----------------------------------------------');
 }
