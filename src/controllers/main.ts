11014a80ad08cb4bc5ffeded894bf86555261dc2
Jackson <159603141+jhbean03@users.noreply.github.com>
2025-02-26 00:05:54 -0500

    Implemented data validation

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -1,14 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 import { Controller } from '../decorators/controller';
 import { Route } from '../decorators/route';
+import { Validate } from '../decorators/validate';
+import Joi from 'joi';
+
+const postHealthCheckValidation = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email(),
+    description: Joi.string()
+});
 
 @Controller()
 class MainController {
     @Route('get', '/healthcheck')
-    @Route('post', '/healthcheck')
     getHealthCheck(req: Request, res: Response, next: NextFunction) {
         console.log('Healthcheck called successfully!');
-        res.status(200).json({ app: "running!", ...req.body });
+        res.status(200).json({ app: "running!" });
+    }
+
+    @Route('post', '/healthcheck')
+    @Validate(postHealthCheckValidation)
+    postHealthCheck(req: Request, res: Response, next: NextFunction) {
+        console.log('Healthcheck called successfully!');
+        res.status(200).json({ ...req.body });
     }
 }
 
