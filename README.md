ae383e6b84e7928e15f70122b788959c37c286ae
Jackson <159603141+jhbean03@users.noreply.github.com>
2025-02-27 23:19:06 -0500

    Configurations for React front end.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FullStackAPI
 Full Stack API assessment for DevAO
+
+1. Install dependencies
+2. Make sure credentials for MongoDB are pasted in .env file (give .env template)
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router } from '@reach/router';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        
+      </Router>
+    </div>
+  );
+}
+
+export default App;
