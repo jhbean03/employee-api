a336bf187fcfc785039a30d547075e9dda322d1d
Jackson <159603141+jhbean03@users.noreply.github.com>
2025-04-26 23:04:43 -0400

    cleaning up frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,37 @@
 import './App.css';
-import { IDBar } from './components/idBar/idBar';
-import { useState } from 'react';
 import { ResultList } from './components/resultList/resultList';
+import React, { useState } from 'react';
 
-interface Employee {
+export interface Employee {
   first_name: string;
   last_name: string;
   skills: string[];
 }
 
-function App() {
-  const [results, setResults] = useState<Employee | null>(null);
+export interface SearchInput {
+  input: string;
+}
 
+function App() {
+  const [searchIn, setSearchIn] = useState({ input: "" });
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchIn({ input: e.target.value })
+  }
+  
   return (
     <div className="App">
       <h1>Employee Database</h1>
-      <p>Please enter an employee ID to obtain information about their related skills below.</p>
-      <IDBar setResults={setResults}/>
-      {!results ? <p>Not Found</p> : <ResultList results={results}></ResultList>}
+      <p>Please enter an employee name to obtain information about them below.</p>
+
+      <div id = "searchArea">
+        <div id = "searchBarWrapper">
+          <input id = "searchBar" placeholder = "Type to search..." value={searchIn.input} onChange={handleSearchChange}/>
+        </div>
+      </div>
+
+      <ResultList input={searchIn.input}></ResultList>
+
     </div>
   );
 }
--- frontend/src/components/resultList/resultList.tsx
+++ frontend/src/components/resultList/resultList.tsx
@@ -1,39 +1,8 @@
 import './resultList.css'
+import { Employee, SearchInput } from '../../App'
 
-interface Employee {
-    first_name: string;
-    last_name: string;
-    skills: string[];
-}
-
-interface ResultListProps {
-    results: Employee | null;
-}
-
-export const ResultList = ({ results } : ResultListProps) => {
-    if (!results || results.first_name == null) {
-        return (
-            <div id = "resultsList">
-                <div id = "error">
-                    Error: Employee information could not be found.
-                </div>
-            </div>
-        );
-    }
-
-    return (
-        <div id = "resultsList">
-            <div id = "employeeName"><b>Employee Name:</b> {results.first_name} {results.last_name}</div>
-            <div id = "skills">
-                <b>Skills:</b>
-                <ul id = "skillsList">
-                    {results.skills?.length > 0 ? (
-                        results.skills.map((skill, index) => <li key={index}>{skill}</li>)
-                    ) : (
-                        <li>No skills listed</li>
-                    )}
-                </ul>
-            </div>
-        </div>
+export const ResultList = ({ input }: SearchInput) => {
+    return(
+        <p>You typed: {input}</p>
     );
 };
