300abd3c48cfd6474407d292509bc04e3800face
jhbean130 <jhbean130@gmail.com>
2025-04-30 13:20:16 -0400

    update repository

--- frontend/src/vite-env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-    readonly VITE_APP_SERVER_PORT: string;
-    readonly VITE_APP_SERVER_HOSTNAME: string;
-}
-  
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
